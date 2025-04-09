2ed3c5f76036e5d1ce88d0626b1b22a0f39ab294
abhishek199677 <puttapoguabhishek1007@gmail.com>
2025-04-09 16:51:02 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'End-to-End-Medical-Chatbot-GenAi',
+    version= '0.0.0',
+    author= 'abhishek199677',
+    author_email= 'puttapoguabhishek1007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
